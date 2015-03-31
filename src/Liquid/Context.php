285c46a72d2178c0c2f2d1af4603cb6b5ebeebd2
Ivan Saranin <ivan@saranin.com>
2015-04-01 03:00:05 +0600

    wrong regex for array if for loop

--- src/Liquid/Context.php
+++ src/Liquid/Context.php
@@ -194,7 +194,8 @@ class Context
 			return $matches[1];
 		}
 
-		if (preg_match('/^\((\S+)..(\S+)\)$/', $key, $matches)) {
+		if (preg_match('/^\((\S+)\.\.(\S+)\)$/', $key, $matches)) {
+			var_dump($key);
 			$min = is_numeric($matches[1])?$matches[1]:$this->variable($matches[1]);
 			$max = is_numeric($matches[2])?$matches[2]:$this->variable($matches[2]);
 			return range($min, $max);
